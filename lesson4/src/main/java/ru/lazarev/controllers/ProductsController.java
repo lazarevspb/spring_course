a3d160bd29ef245c58591c136207daadc29e679b
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-03-13 16:08:32 +0300

    added json_view.html, xml_view.html

--- lesson4/src/main/java/ru/lazarev/controllers/ProductsController.java
+++ lesson4/src/main/java/ru/lazarev/controllers/ProductsController.java
@@ -1,5 +1,7 @@
 package ru.lazarev.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +9,9 @@ import ru.lazarev.exception.ResourceNotFoundException;
 import ru.lazarev.model.Product;
 import ru.lazarev.services.ProductsService;
 
+import javax.xml.bind.JAXB;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Optional;
 
 @Controller
@@ -53,6 +58,26 @@ public class ProductsController {
         productsService.saveOrUpdate(product);
         System.out.println(productsService.toString());
         return "redirect:/products";
+    }
+
+    @GetMapping("get_json/{id}")
+    public String getJson(Model model, @PathVariable Long id) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Продукт не найден"));
+        String jsonString = objectMapper.writeValueAsString(product);
+        model.addAttribute("receivedText", jsonString);
+        return "json_view";
+    }
+
+
+    @GetMapping("get_xml/{id}")
+    public String getXML(Model model, @PathVariable Long id) {
+        Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Продукт не найден"));
+        StringWriter sw = new StringWriter();
+        JAXB.marshal(product, sw);
+        String xmlString = sw.toString();
+        model.addAttribute("xmlString", xmlString);
+        return "xml_view";
     }
 
 }
