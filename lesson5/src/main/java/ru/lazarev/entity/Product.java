2898f0feb1d5cdb803865492ece765a8132e0846
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-03-14 19:12:47 +0300

    wip

--- lesson5/src/main/java/ru/lazarev/entity/Product.java
+++ lesson5/src/main/java/ru/lazarev/entity/Product.java
@@ -3,25 +3,60 @@ package ru.lazarev.entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@NoArgsConstructor
-@AllArgsConstructor
+import javax.persistence.*;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Entity
-@Table(name ="products")
+@Table(name ="products", schema = "hibernate")
+@NamedQueries({
+        @NamedQuery(name = "Product.finAll", query = "SELECT p FROM Product p")
+})
 public class Product {
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
+    @Column(name = "id", unique=true,nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
     private Long id;
     @Column(name = "name", length = 128)
     private String name;
+    @Column(name = "price")
     private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 
     @Override
     public String toString() {
-        return String.format("id: %s, name: %s, price: %s", id, name, price);
+        return String.format("id: %d, name: %s, price: %d", id, name, price);
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
