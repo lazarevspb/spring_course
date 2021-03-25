358e428eeea3f3548771e1943d91a148fb7294db
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-03-25 23:09:51 +0300

    l8 fix ProductController.java, ProductRepository.java< ProductServices.java

--- lesson8/src/main/java/ru/lazarev/lesson8/Lesson8Application.java
+++ lesson8/src/main/java/ru/lazarev/lesson8/Lesson8Application.java
@@ -3,6 +3,12 @@ package ru.lazarev.lesson8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Homework for lesson #8
+ *
+ * @author Valeriy Lazarev
+ * @since 25.03.2021
+ */
 @SpringBootApplication
 public class Lesson8Application {
 	public static void main(String[] args) {
--- lesson8/src/main/java/ru/lazarev/lesson8/repositories/ProductRepository.java
+++ lesson8/src/main/java/ru/lazarev/lesson8/repositories/ProductRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.lazarev.lesson8.model.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-
-
+    List<Product> findAllByPriceBetween(int min, int max);
 }
