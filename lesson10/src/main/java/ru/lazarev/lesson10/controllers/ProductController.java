c7bd630a3ae21b0354447c89e325cfe5a9b7d76a
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-03-31 20:43:35 +0300

    l10 init

---
+++ lesson10/src/main/java/ru/lazarev/lesson10/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package ru.lazarev.lesson10.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import ru.lazarev.lesson10.dto.ProductDto;
+import ru.lazarev.lesson10.model.Product;
+import ru.lazarev.lesson10.repositories.specifications.ProductSpecifications;
+import ru.lazarev.lesson10.services.ProductServices;
+
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductServices services;
+
+    //`http://localhost:8189/market/index.html`
+    @GetMapping
+    public Page<ProductDto> findAllProducts(
+            @RequestParam MultiValueMap<String, String> params,
+            @RequestParam(name = "p", defaultValue = "1") Integer page
+    ) {
+        if (page < 1) {
+            page = 1;
+        }
+        return services.findAll(ProductSpecifications.build(params), page, 5);
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return services.findProductById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product saveNewProduct(@RequestBody Product product) {
+        product.setId(null);
+        return services.saveOrUpdate(product);
+    }
+
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product) {
+        return services.saveOrUpdate(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductById(@PathVariable Long id) {
+        services.deleteProductById(id);
+    }
+}
